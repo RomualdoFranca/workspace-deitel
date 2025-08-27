0f49d7bbdfa03e9a4cb4b428eb6f28180962248b
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-08-27 09:59:05 -0300

    implementa a subclasse BasePlusCommissionEmployee

---
+++ capitulo09/src/capitulo09/exemplo/entities/BasePlusCommissionEmployeeTest.java
@@ -0,0 +1,12 @@
+package capitulo09.exemplo.entities;
+
+public class BasePlusCommissionEmployeeTest {
+	
+	public static void main(String[] args) {
+		
+		BasePlusCommissionEmployee employee = new BasePlusCommissionEmployee(
+				"Bob", "Lewis", "333-333-33", 5000, .04, 300);
+		System.out.println(employee);
+		
+	}
+}// fim da classe
